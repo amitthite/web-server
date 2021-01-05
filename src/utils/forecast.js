345994fd09f123484fcb6ce64cfb090f98df8a27
AMIT THITE <amithite@in.ibm.com>
2021-01-05 17:23:03 +0530

    first commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,19 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'https://api.darksky.net/forecast/9d1465c6f3bb7a6c71944bdd8548d026/' + latitude + ',' + longitude
+    // const url = 'https://www.darksky.net/forecast/' + latitude + ',' + longitude
+    console.log('Amit log : Forecast.js url =' + url)
+    request({ url, json: true }, (error,  body ) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined)
+        } else if (body.error) {
+            callback('Unable to find location', undefined)
+        } else {
+            console.log('amit log : forecast.js body --> '+ body)
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+        }
+    })
+}
+
+module.exports = forecast
---
+++ src/utils/forecast2.js
@@ -0,0 +1,30 @@
+const request = require('request')
+
+const forecast2 = (address, callback) => {
+const options = {
+  method: 'GET',
+  url: 'https://community-open-weather-map.p.rapidapi.com/weather',
+  qs: {q: address, lat: '0', lon: '0', id: '2172797', lang: 'null'},
+  headers: {
+    'x-rapidapi-key': '33e1262b54msh8bed7a0bdd7284cp11f524jsn6e593457f5a1',
+    'x-rapidapi-host': 'community-open-weather-map.p.rapidapi.com',
+    useQueryString: true
+  }
+}
+
+request(options, (error, {body} ) => {
+		if (error) {
+			callback('Unable to connect to location services!', undefined)
+		} else if (body === 404) {
+			callback(`Unable to find location. Try another search.${body.test.message}`, undefined)
+		} else {
+			console.log('amit log : forecast2.js body --> ' + body)
+			bodyString = JSON.parse(body)
+			bodyString.main.temp = bodyString.main.temp - 273
+			bodyString.main.feels_like = bodyString.main.feels_like - 273
+			callback(undefined, ' It is currently ' + bodyString.main.temp + ' degress out. It feels like ' + bodyString.main.feels_like + ' degrees.')
+		}
+	}
+)
+}
+module.exports = forecast2
